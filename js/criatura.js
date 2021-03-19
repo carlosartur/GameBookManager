3e52b9273bc61ad2e8ebb8ca1e4b03ca927db354
Carlos Artur Neubert Gonçalves <carlos.goncalves@flexy.com.br>
2021-03-19 12:58:17 -0300

    fix: Remove babeljs es5 class conversion prior to es6 classes

--- js/criatura.js
+++ js/criatura.js
@@ -1,98 +1,38 @@
-try {
-    "use strict";
+class Criatura {
 
-    class Criatura {
-
-        constructor (mensagemMorte) {
-            this.mensagemMorte = mensagemMorte;
-        }
-
-        addHabi () {
-            this.habilidade++;
-        }
-
-        subHab () {
-            if (this.habilidade <= 1) {
-                alert("A habilidade não pode ser menor que 1.");
-                return false;
-            } else {
-                this.habilidade--;
-                return true;
-            }
-        }
-
-        addEner (add) {
-            this.energia += add;
-        }
-
-        subEner (sub) {
-            if (this.energia <= 0) {
-                alet(this.mensagemMorte);
-                return false;
-            } else {
-                this.energia -= sub;
-                return true;
-            }
-        }
-
-        ataque () {
-            return (this.habilidade * 1) + random(11) + 1;
-        };
+    constructor (mensagemMorte) {
+        this.mensagemMorte = mensagemMorte;
     }
-} catch (Exception E) {
-    console.log(E);
-    "use strict";
 
-    var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+    addHabi () {
+        this.habilidade++;
+    }
 
-    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-    var Criatura = function () {
-        function Criatura(mensagemMorte) {
-            _classCallCheck(this, Criatura);
-
-            this.mensagemMorte = mensagemMorte;
+    subHab () {
+        if (this.habilidade <= 1) {
+            alert("A habilidade não pode ser menor que 1.");
+            return false;
+        } else {
+            this.habilidade--;
+            return true;
         }
+    }
 
-        _createClass(Criatura, [{
-            key: "addHabi",
-            value: function addHabi() {
-                this.habilidade++;
-            }
-        }, {
-            key: "subHab",
-            value: function subHab() {
-                if (this.habilidade <= 1) {
-                    alert("A habilidade não pode ser menor que 1.");
-                    return false;
-                } else {
-                    this.habilidade--;
-                    return true;
-                }
-            }
-        }, {
-            key: "addEner",
-            value: function addEner(add) {
-                this.energia += add;
-            }
-        }, {
-            key: "subEner",
-            value: function subEner(sub) {
-                if (this.energia <= 0) {
-                    alet(this.mensagemMorte);
-                    return false;
-                } else {
-                    this.energia -= sub;
-                    return true;
-                }
-            }
-        }, {
-            key: "ataque",
-            value: function ataque() {
-                return this.habilidade * 1 + random(11) + 1;
-            }
-        }]);
+    addEner (add) {
+        this.energia += add;
+    }
 
-        return Criatura;
-    }();
+    subEner (sub) {
+        if (this.energia <= 0) {
+            alet(this.mensagemMorte);
+            return false;
+        } else {
+            this.energia -= sub;
+            return true;
+        }
+    }
+
+    ataque () {
+        return (this.habilidade * 1) + random(11) + 1;
+    };
 }
--- js/monstro.js
+++ js/monstro.js
@@ -1,35 +1,7 @@
-try {
-	"use strict";
-
-	class Monstro extends Criatura{
-	    constructor(energia, habilidade) {
-	        super("Você morreu!");
-	        this.energia = energia;
-	        this.habilidade = habilidade;
-	    }
+class Monstro extends Criatura{
+	constructor(energia, habilidade) {
+		super("Você morreu!");
+		this.energia = energia;
+		this.habilidade = habilidade;
 	}
-} catch (Exception E) {
-	"use strict";
-
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-	var Monstro = function (_Criatura) {
-	    _inherits(Monstro, _Criatura);
-
-	    function Monstro(energia, habilidade) {
-	        _classCallCheck(this, Monstro);
-
-	        var _this = _possibleConstructorReturn(this, (Monstro.__proto__ || Object.getPrototypeOf(Monstro)).call(this, "Você morreu!"));
-
-	        _this.energia = energia;
-	        _this.habilidade = habilidade;
-	        return _this;
-	    }
-
-	    return Monstro;
-	}(Criatura);
 }
